3483546d71f2a72b29c966ee0670c56137b61eb8
chin5270 <crazy23123456@gmail.com>
2023-10-17 21:37:35 +0800

    MyViewModel viewBinding test

--- app/src/main/java/com/example/cbmoney/EditNoteFragment.java
+++ app/src/main/java/com/example/cbmoney/EditNoteFragment.java
@@ -1,6 +1,7 @@
 package com.example.cbmoney;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -11,18 +12,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cbmoney.ViewModel.MyViewModel;
-import com.example.cbmoney.databinding.ActivityMainBinding;
+
 import com.example.cbmoney.databinding.FragmentEditNoteBinding;
 
 
 public class EditNoteFragment extends Fragment {
     MyViewModel myViewModel;
-    FragmentEditNoteBinding binding;
+    private FragmentEditNoteBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         binding = FragmentEditNoteBinding.inflate(inflater, container, false);
+
+        myViewModel= new ViewModelProvider(requireActivity()).get(MyViewModel.class);
+        myViewModel.getNumber().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                binding.texviewShownum.setText(String.valueOf(integer));
+                Log.d("chin","onChanged:myViewModel.getNumber():"+myViewModel.getNumber().getValue());
+
+            }
+        });
+
+        binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myViewModel.add();
+            }
+        });
+
+
         return binding.getRoot();
     }
+
+
 
 }
