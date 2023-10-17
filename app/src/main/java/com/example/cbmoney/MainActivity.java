023c02a41c98a72c0382da7bbca8f73e6fd42f84
chin5270 <crazy23123456@gmail.com>
2023-10-17 11:39:09 +0800

    Bottom Navigation Bar

--- app/src/main/java/com/example/cbmoney/MainActivity.java
+++ app/src/main/java/com/example/cbmoney/MainActivity.java
@@ -1,16 +1,48 @@
 package com.example.cbmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.cbmoney.R;
+import com.example.cbmoney.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setContentView(binding.getRoot());
+        // 一開始的畫面
+        replaceFragment(new EditNoteFragment());
+        // 監聽按鍵被觸發時，做得動作
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id=item.getItemId();
+            if (id == R.id.edit_note) {
+                replaceFragment(new EditNoteFragment());
+            } else if (id == R.id.account) {
+                replaceFragment(new AccountFragment());
+            } else if (id == R.id.pie_chart) {
+                replaceFragment(new PieChartFragment());
+            }
+            return true;
+        });
     }
+
+    // 切換畫面
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fmrg = getSupportFragmentManager();
+        FragmentTransaction transaction = fmrg.beginTransaction();
+        transaction.replace(R.id.frameLayout,fragment);
+        transaction.commit();
+
+    }
+
 }
